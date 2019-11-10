bd5b582db3286cd6e4b2a355697746cfe382dbc3
pretraket <43110626+pretraket@users.noreply.github.com>
2019-11-10 01:16:40 +0100

    improved russian translation!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var open = false;
 
-$(".navbutton").click(function() {
+$(".navbutton").click(function () {
   console.log("succes");
   $(".navbutton__line--1").toggleClass("move-down");
   $(".navbutton__line--2").toggleClass("display-toggle");
@@ -22,7 +22,7 @@ $(".navbutton").click(function() {
 
 
 
-$("#social").click(function() {
+$("#social").click(function () {
 
   if ($(".popup--2").css("opacity") == 1) {
     $(".popup--2").toggleClass("opacity-toggle");
@@ -47,7 +47,7 @@ $("#social").click(function() {
 });
 
 
-$("#design").click(function() {
+$("#design").click(function () {
 
   if ($(".popup--1").css("opacity") == 1) {
     $(".popup--1").toggleClass("opacity-toggle");
@@ -74,7 +74,7 @@ $("#design").click(function() {
 
 
 
-$("#userinter").click(function() {
+$("#userinter").click(function () {
 
 
   if ($(".popup--1").css("opacity") == 1) {
@@ -98,7 +98,7 @@ $("#userinter").click(function() {
 
 });
 
-$("#contact-ons").click(function() {
+$("#contact-ons").click(function () {
 
 
   if ($(".popup--1").css("opacity") == 1) {
@@ -123,7 +123,7 @@ $("#contact-ons").click(function() {
 });
 
 
-$("#ons-projekt").click(function() {
+$("#ons-projekt").click(function () {
 
 
   if ($(".popup--1").css("opacity") == 1) {
@@ -148,7 +148,7 @@ $("#ons-projekt").click(function() {
 });
 
 
-$("#onze-info").click(function() {
+$("#onze-info").click(function () {
 
 
   if ($(".popup--1").css("opacity") == 1) {
@@ -177,7 +177,7 @@ $("#onze-info").click(function() {
 
 
 
-$("#kontakt").click(function() {
+$("#kontakt").click(function () {
   $("#icon-1").toggleClass("turn");
   $("#list1").toggleClass("header-enlarge");
   $("#kontakt").toggleClass("no-radius");
@@ -188,7 +188,7 @@ $("#kontakt").click(function() {
 });
 
 
-$("#list2").click(function() {
+$("#list2").click(function () {
 
 
   $("#icon-2").toggleClass("turn");
@@ -201,7 +201,7 @@ $("#list2").click(function() {
 });
 
 
-$("#list3").click(function() {
+$("#list3").click(function () {
   $("#icon-3").toggleClass("turn");
   $("#list3").toggleClass("header-enlarge");
   $("#info").toggleClass("no-radius");
@@ -216,40 +216,40 @@ $("#list3").click(function() {
 
 
 
-$("#close1").click(function() {
+$("#close1").click(function () {
   if ($(".popup--1").css("opacity") == 1) {
     $(".popup--1").toggleClass("opacity-toggle");
     $(".popup--1").toggleClass("highz");
   }
 });
 
-$("#close2").click(function() {
+$("#close2").click(function () {
   if ($(".popup--2").css("opacity") == 1) {
     $(".popup--2").toggleClass("opacity-toggle");
     $(".popup--2").toggleClass("highz");
   }
 });
 
-$("#close3").click(function() {
+$("#close3").click(function () {
   if ($(".popup--3").css("opacity") == 1) {
     $(".popup--3").toggleClass("opacity-toggle");
     $(".popup--3").toggleClass("highz");
   }
 });
 
-$("#close4").click(function() {
+$("#close4").click(function () {
   if ($(".popup--4").css("opacity") == 1) {
     $(".popup--4").toggleClass("opacity-toggle");
     $(".popup--4").toggleClass("highz");
   }
 });
-$("#close5").click(function() {
+$("#close5").click(function () {
   if ($(".popup--5").css("opacity") == 1) {
     $(".popup--5").toggleClass("opacity-toggle");
     $(".popup--5").toggleClass("highz");
   }
 });
-$("#close6").click(function() {
+$("#close6").click(function () {
   if ($(".popup--6").css("opacity") == 1) {
     $(".popup--6").toggleClass("opacity-toggle");
     $(".popup--6").toggleClass("highz");
@@ -258,66 +258,66 @@ $("#close6").click(function() {
 
 
 
-$("#nl").click(function(){
+$("#nl").click(function () {
 
-if( $(".language__option--1").css("opacity") === "0.999"){
-  $("#nl").toggleClass("active__translation");
-
-
- $(".language__option--2").removeClass("active__translation");
-  $(".language__option--3").removeClass("active__translation");
-}
+  if ($(".language__option--1").css("opacity") === "0.999") {
+    $("#nl").toggleClass("active__translation");
 
 
+    $(".language__option--2").removeClass("active__translation");
+    $(".language__option--3").removeClass("active__translation");
+  }
 
 
-// INFOBOX
-$(".infobox__item--1").html("Website Design op Maat <hr>");
-$(".infobox__item--2").html("Eerlijke Tarieven <hr>");
-$(".infobox__item--3").html("Gratis eerste Consult <hr>");
-
-//SIDEBAR ITEMS
-$("#social").html("<a>Sociale Media</a>");
-$("#design").html("<a>Web Design Cursus</a>");
-$("#userinter").html("<a>Web Design</a>");
-$("#contact-ons").html("<a>Contact</a>");
-$("#ons-projekt").html("<a>Projecten</a>");
-$("#onze-info").html("<a>Info</a>");
 
 
-//POPUP TWO
-$(".popup--2__text--1").text("Altijd al willen leren om websites te bouwen?");
-$(".price__tag").text("Per uur");
-$(".price__disclaimer").text("(€10- voor 30 minuten)");
-$(".popup--2__translate--1").text("Een uur 1 op 1 les via video");
-$(".popup--2__translate--2").text("Bouw werkende sites vanaf de eerste les");
-$(".popup--2__translate--3").text("Buiten lessen om worden vragen ook beantwoord");
-$(".popup--2__translate--4").text("Bij meerdere lessen wordt korting gerekend");
+  // INFOBOX
+  $(".infobox__item--1").html("Website Design op Maat <hr>");
+  $(".infobox__item--2").html("Eerlijke Tarieven <hr>");
+  $(".infobox__item--3").html("Gratis eerste Consult <hr>");
 
-//POPUP THREE
-$(".popup--3__text--1").text("Wilt u:");
-$(".popup--3__translate--1").text("Een moderne interface?");
-$(".popup--3__translate--2").text("Een website die er ook mooi uitziet op een telefoon?");
-$(".popup--3__translate--3").text("Een website die professionaliteit uitstraalt?");
-$(".popup--3__footer").text("Neem contact op voor een intake gesprek!");
+  //SIDEBAR ITEMS
+  $("#social").html("<a>Sociale Media</a>");
+  $("#design").html("<a>Web Design Cursus</a>");
+  $("#userinter").html("<a>Web Design</a>");
+  $("#contact-ons").html("<a>Contact</a>");
+  $("#ons-projekt").html("<a>Projecten</a>");
+  $("#onze-info").html("<a>Info</a>");
 
 
-//POPUP SIX
-$(".popup--6__translate--1").text("HTML CSS en JavaScript");
-$(".popup--6__translate--2").text("Backend met NodeJS");
-$(".popup--6__translate--3").text("MongoDB databases");
-$(".popup--6__translate--4").text("E-Commerce");
-$(".popup--6__translate--5").text("Modern Design");
+  //POPUP TWO
+  $(".popup--2__text--1").text("Altijd al willen leren om websites te bouwen?");
+  $(".price__tag").text("Per uur");
+  $(".price__disclaimer").text("(€10- voor 30 minuten)");
+  $(".popup--2__translate--1").text("Een uur 1 op 1 les via video");
+  $(".popup--2__translate--2").text("Bouw werkende sites vanaf de eerste les");
+  $(".popup--2__translate--3").text("Buiten lessen om worden vragen ook beantwoord");
+  $(".popup--2__translate--4").text("Bij meerdere lessen wordt korting gerekend");
+
+  //POPUP THREE
+  $(".popup--3__text--1").text("Wilt u:");
+  $(".popup--3__translate--1").text("Een moderne interface?");
+  $(".popup--3__translate--2").text("Een website die er ook mooi uitziet op een telefoon?");
+  $(".popup--3__translate--3").text("Een website die professionaliteit uitstraalt?");
+  $(".popup--3__footer").text("Neem contact op voor een intake gesprek!");
+
+
+  //POPUP SIX
+  $(".popup--6__translate--1").text("HTML CSS en JavaScript");
+  $(".popup--6__translate--2").text("Backend met NodeJS");
+  $(".popup--6__translate--3").text("MongoDB databases");
+  $(".popup--6__translate--4").text("E-Commerce");
+  $(".popup--6__translate--5").text("Modern Design");
 });
 
 
-$("#en").click(function(){
+$("#en").click(function () {
 
-  if( $(".language__option--2").css("opacity") === "0.999"){
+  if ($(".language__option--2").css("opacity") === "0.999") {
     $("#en").toggleClass("active__translation");
 
 
-   $(".language__option--1").removeClass("active__translation");
+    $(".language__option--1").removeClass("active__translation");
     $(".language__option--3").removeClass("active__translation");
   }
 
@@ -327,42 +327,42 @@ $("#en").click(function(){
 
 
 
-// INFOBOX
-$(".infobox__item--1").html("Personalised Web Design <hr>");
-$(".infobox__item--2").html("Fair Prices <hr>");
-$(".infobox__item--3").html("Free Intake <hr>");
+  // INFOBOX
+  $(".infobox__item--1").html("Personalised Web Design <hr>");
+  $(".infobox__item--2").html("Fair Prices <hr>");
+  $(".infobox__item--3").html("Free Intake <hr>");
 
-//SIDEBAR ITEMS
-$("#social").html("<a>Social Media</a>");
-$("#design").html("<a>Web Design Course</a>");
-$("#userinter").html("<a>Web Design</a>");
-$("#contact-ons").html("<a>Contact</a>");
-$("#ons-projekt").html("<a>Projects</a>");
-$("#onze-info").html("<a>Info</a>");
+  //SIDEBAR ITEMS
+  $("#social").html("<a>Social Media</a>");
+  $("#design").html("<a>Web Design Course</a>");
+  $("#userinter").html("<a>Web Design</a>");
+  $("#contact-ons").html("<a>Contact</a>");
+  $("#ons-projekt").html("<a>Projects</a>");
+  $("#onze-info").html("<a>Info</a>");
 
 
-//POPUP TWO
-$(".popup--2__text--1").text("Always wanted to learn how to make a website?");
-$(".price__tag").text("Per hour");
-$(".price__disclaimer").text("(€10- for 30 minutes)");
-$(".popup--2__translate--1").text("An hour 1 on 1 video lesson");
-$(".popup--2__translate--2").text("Build working sites in the first lesson ");
-$(".popup--2__translate--3").text("Questions are answered outside lessons aswell");
-$(".popup--2__translate--4").text("Get a discount when buying multiple lessons");
+  //POPUP TWO
+  $(".popup--2__text--1").text("Always wanted to learn how to make a website?");
+  $(".price__tag").text("Per hour");
+  $(".price__disclaimer").text("(€10- for 30 minutes)");
+  $(".popup--2__translate--1").text("An hour 1 on 1 video lesson");
+  $(".popup--2__translate--2").text("Build working sites in the first lesson ");
+  $(".popup--2__translate--3").text("Questions are answered outside lessons aswell");
+  $(".popup--2__translate--4").text("Get a discount when buying multiple lessons");
 
-//POPUP THREE
-$(".popup--3__text--1").text("Do you  want:");
-$(".popup--3__translate--1").text("A modern interface?");
-$(".popup--3__translate--2").text("A website that looks good on a mobile device aswell?");
-$(".popup--3__translate--3").text("A website that exudes professionality?");
-$(".popup--3__footer").text("Contact us for an intake!");
+  //POPUP THREE
+  $(".popup--3__text--1").text("Do you  want:");
+  $(".popup--3__translate--1").text("A modern interface?");
+  $(".popup--3__translate--2").text("A website that looks good on a mobile device aswell?");
+  $(".popup--3__translate--3").text("A website that exudes professionality?");
+  $(".popup--3__footer").text("Contact us for an intake!");
 
-//POPUP SIX
-$(".popup--6__translate--1").text("HTML CSS and JavaScript");
-$(".popup--6__translate--2").text("Backend with NodeJS");
-$(".popup--6__translate--3").text("MongoDB databases");
-$(".popup--6__translate--4").text("E-Commerce");
-$(".popup--6__translate--5").text("Modern Design");
+  //POPUP SIX
+  $(".popup--6__translate--1").text("HTML CSS and JavaScript");
+  $(".popup--6__translate--2").text("Backend with NodeJS");
+  $(".popup--6__translate--3").text("MongoDB databases");
+  $(".popup--6__translate--4").text("E-Commerce");
+  $(".popup--6__translate--5").text("Modern Design");
 
 
 
@@ -376,13 +376,13 @@ $(".popup--6__translate--5").text("Modern Design");
 
 
 
-$("#ru").click(function(){
+$("#ru").click(function () {
 
-  if( $(".language__option--3").css("opacity") === "0.999"){
+  if ($(".language__option--3").css("opacity") === "0.999") {
     $("#ru").toggleClass("active__translation");
 
 
-   $(".language__option--1").removeClass("active__translation");
+    $(".language__option--1").removeClass("active__translation");
     $(".language__option--2").removeClass("active__translation");
   }
 
@@ -391,34 +391,34 @@ $("#ru").click(function(){
 
 
   // INFOBOX
-  $(".infobox__item--1").html("индивидуальный дизайн сайта <hr>");
-  $(".infobox__item--2").html("справедливые цены <hr>");
-  $(".infobox__item--3").html("бесплатный первый прием <hr>");
+  $(".infobox__item--1").html("Индивидуальный дизайн сайта <hr>");
+  $(".infobox__item--2").html("Справедливые цены <hr>");
+  $(".infobox__item--3").html("Бесплатный первый прием <hr>");
 
   //SIDEBAR ITEMS
   $("#social").html("<a>социальные сети</a>");
-  $("#design").html("<a>курс дизайна сайтаs</a>");
+  $("#design").html("<a>курс веб-дизайна</a>");
   $("#userinter").html("<a>веб-дизайн</a>");
   $("#contact-ons").html("<a>контакты</a>");
-  $("#ons-projekt").html("<a>проектов</a>");
+  $("#ons-projekt").html("<a>проекты</a>");
   $("#onze-info").html("<a>Информация</a>");
 
 
   //POPUP TWO
-  $(".popup--2__text--1").text("всегда хотел создавать собственные сайты?");
+  $(".popup--2__text--1").text("Всегда хотели создавать собственные сайты?");
   $(".price__tag").text("в час");
   $(".price__disclaimer").text("(€10- 30 минут)");
-  $(".popup--2__translate--1").text("видео урок один на один (на английском)");
-  $(".popup--2__translate--2").text("создавать рабочие сайты с вашего первого урока!");
-  $(".popup--2__translate--3").text("на вопросы вне уроков тоже будут даны ответы");
-  $(".popup--2__translate--4").text("при заказе нескольких уроков предоставляется скидка");
+  $(".popup--2__translate--1").text("Индивидуальный онлайн урок (на английском)");
+  $(".popup--2__translate--2").text("Вы сможете создавать рабочие сайты уже после первого урока!");
+  $(".popup--2__translate--3").text("Ответы на все интересующие вопросы");
+  $(".popup--2__translate--4").text("При покупке пакета уроков предоставляется скидка");
 
   //POPUP THREE
   $(".popup--3__text--1").text("вы хотите:");
   $(".popup--3__translate--1").text("современный интерфейс?");
-  $(".popup--3__translate--2").text("сайт, который также хорошо выглядит на телефоне??");
-  $(".popup--3__translate--3").text("сайт, который излучает профессионализм?");
-  $(".popup--3__footer").text("напишите или позвоните нам для приема!");
+  $(".popup--3__translate--2").text("Сайт, который так же хорошо выглядит на телефоне?");
+  $(".popup--3__translate--3").text("Сайт, который излучает профессионализм?");
+  $(".popup--3__footer").text("Свяжитесь с нами для обсуждения деталей");
 
 
   //POPUP SIX
@@ -426,7 +426,7 @@ $("#ru").click(function(){
   $(".popup--6__translate--2").text("Backend с NodeJS");
   $(".popup--6__translate--3").text("MongoDB databases");
   $(".popup--6__translate--4").text("E-Commerce");
-  $(".popup--6__translate--5").text("современный веб-дизайн");
+  $(".popup--6__translate--5").text("Современный веб-дизайн");
 
 
 
